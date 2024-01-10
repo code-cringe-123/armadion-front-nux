2967fcc925fac69dba909b1cc3427f3fa8783bd3
Hellpes <alexrui_hpow@mail.ru>
2024-01-10 21:28:09 +0300

    back catalog

--- pages/Catalog.vue
+++ pages/Catalog.vue
@@ -4,63 +4,44 @@
     <Catalog :products="products" :products_filtered="products_filtered" />
   </div>
 </template>
-
 <script setup>
 import { ref, watchEffect } from "vue";
 const products = ref([]);
 const products_filtered = ref([]);
 const filters = ref([]);
-
 const filterQuery = new URLSearchParams();
-
-async function onFilterUpdate(key_key, value_value_value) {
+function onFilterUpdate(key_key, value_value_value) {
   filterQuery.append(key_key, value_value_value);
-  refresh(); 
+  refresh();
 }
-
-let { data, refresh } = await useFetch(
+const { data, refresh } = await useFetch(
   `https://api-armadion.ru/doors/filter?${filterQuery.toString()}`,
 );
-
-filters.value = data.value.filters;
-
-watchEffect(async () => {
-  refresh();
+watchEffect(() => {
   for (let [key, value] of filterQuery.entries()) {
     console.log(key, value);
+    
+    products.value.push(
+      ...`https://api-armadion.ru/doors/filter?${filterQuery.toString()}`.value
+        .doors,
+    );
   }
-  data = await useFetch(
-    `https://api-armadion.ru/doors/filter?${filterQuery.toString()}`,
-  );
-  
-  
-  // console.log(1)
-  // console.log(data?.data?._value?.doors);
-  // for (var key1 in data) {
-  //   if (key1 === "data"){
-  //     for (var key_key2 in data[key1]){
-  //       if (key_key2 === "_value"){
-  //         for (var key_key_key3 in data[key1][key_key2]){
-  //           if (key_key_key3 === "doors"){
-  //             console.log(data[key1][key_key2][key_key_key3])
-  //             console.log(`${key1}, ${key_key2}, ${key_key_key3}`)
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
+  // products.value = [...data.value.doors];
+  filters.value = data.value.filters;
+  // try {
+  //   products.value = [...data.value.doors];
+  //   console.log(products)
+  // } catch (error) {
+  //   console.error("Произошла ошибка:", error);
   // }
-  console.log(data)
-  console.log(`https://api-armadion.ru/doors/filter?${filterQuery.toString()}`)
-  console.log(data?.data?._value?.doors)
-  products.value.push(...(data?.data?._value?.doors || []));
-
-
-  filters.value = data?.data?._value?.filters;
-
+  // // products.value.push(...data.value.doors);
+  // console.log("HERE");
+  // // console.log( `https://api-armadion.ru/doors/filter?${filterQuery.toString()}`)
+  // console.log(data);
+  // console.log(products);
 });
 </script>
-
+  
 <style lang="scss">
 .catalog-page-container {
   margin-top: 195px;
