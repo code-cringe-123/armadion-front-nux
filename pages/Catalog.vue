e7bce0c06c93a720054cd5b3b4f00480088d9566
Hellpes <alexrui_hpow@mail.ru>
2024-01-10 19:46:21 +0300

    test1

--- pages/Catalog.vue
+++ pages/Catalog.vue
@@ -22,39 +22,41 @@ let { data, refresh } = await useFetch(
   `https://api-armadion.ru/doors/filter?${filterQuery.toString()}`,
 );
 
-watchEffect(async () => {
-  refresh();
-  for (let [key, value] of filterQuery.entries()) {
-    // console.log(key, value);
-    // data = await useFetch(
-    // `https://api-armadion.ru/doors/filter?${filterQuery.toString()}`,
-    // );
-  }
+filters.value = data.value.filters;
+
+// watchEffect(async () => {
+//   refresh();
+//   for (let [key, value] of filterQuery.entries()) {
+//     // console.log(key, value);
+//     // data = await useFetch(
+//     // `https://api-armadion.ru/doors/filter?${filterQuery.toString()}`,
+//     // );
+//   }
 
   
   
-  // console.log(1)
-  // console.log(data?.data?._value?.doors);
-  // for (var key1 in data) {
-  //   if (key1 === "data"){
-  //     for (var key_key2 in data[key1]){
-  //       if (key_key2 === "_value"){
-  //         for (var key_key_key3 in data[key1][key_key2]){
-  //           if (key_key_key3 === "doors"){
-  //             console.log(data[key1][key_key2][key_key_key3])
-  //             console.log(`${key1}, ${key_key2}, ${key_key_key3}`)
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  console.log(data)
-  console.log(data?.data?._value?.doors)
-  products.value = data?.data?._value?.doors;
-  filters.value = data.value.filters;
+//   // console.log(1)
+//   // console.log(data?.data?._value?.doors);
+//   // for (var key1 in data) {
+//   //   if (key1 === "data"){
+//   //     for (var key_key2 in data[key1]){
+//   //       if (key_key2 === "_value"){
+//   //         for (var key_key_key3 in data[key1][key_key2]){
+//   //           if (key_key_key3 === "doors"){
+//   //             console.log(data[key1][key_key2][key_key_key3])
+//   //             console.log(`${key1}, ${key_key2}, ${key_key_key3}`)
+//   //           }
+//   //         }
+//   //       }
+//   //     }
+//   //   }
+//   // }
+//   console.log(data)
+//   console.log(data?.data?._value?.doors)
+//   products.value = data?.data?._value?.doors;
+//   filters.value = data.value.filters;
 
-});
+// });
 </script>
 
 <style lang="scss">
