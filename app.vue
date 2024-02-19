3aadbd0479ee2802514edeb3098a7374ea28e184
mrclovv66 <154814252+mrclovv@users.noreply.github.com>
2024-02-19 08:51:41 +0300

    new commit

--- app.vue
+++ app.vue
@@ -4,22 +4,23 @@
       <NuxtPage />
     </NuxtLayout>
   </div>
-  <script>
-  <link rel="manifest" href="manifest.json">
-if (‘serviceWorker’ in navigator) {
- window.addEventListener(‘load’, function() {  
-   navigator.serviceWorker.register(‘/sw.js’).then(
-     function(registration) {
-       // Registration was successful
-       console.log(‘ServiceWorker registration successful with scope: ‘, registration.scope); },
-     function(err) {
-       // registration failed :(
-       console.log(‘ServiceWorker registration failed: ‘, err);
-     });
- });
-}
-</script>
+  <!-- <link rel="manifest" href="manifest.json"> -->
 </template>
+
+<script setup>
+// if (‘serviceWorker’ in navigator) {
+// window.addEventListener(‘load’, function() {  
+//  navigator.serviceWorker.register(‘/sw.js’).then(
+//    function(registration) {
+//      // Registration was successful
+//      console.log(‘ServiceWorker registration successful with scope: ‘, registration.scope); },
+//    function(err) {
+//      // registration failed :(
+//      console.log(‘ServiceWorker registration failed: ‘, err);
+//    });
+// });
+// }
+</script>
 
 
 <style>
